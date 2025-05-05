dbcbae7c2da2ded892a572be428f27ba36f72b6f
Ahmed <ahmed.mca18@gmail.com>
2025-05-05 12:36:09 +0530

    adding streamlit ui features

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import logging
 import os
 from gtts import gTTS
 import google.generativeai as genai
+import streamlit as st
 import datetime
 
 
@@ -59,6 +60,29 @@ def gemini_model(user_input):
 #     print(f"  Supported methods: {model.supported_generation_methods}")
 #     print("-" * 40)    
 
-text = takeCommand()
-res = gemini_model(text)
-print(res)
+def main():
+    st.title("Multilingual AI Assistant")
+    if st.button("Ask me anything!"):
+        with st.spinner("Listening.."):
+            text = takeCommand()
+            response = gemini_model(text)
+            text_to_speech(response)
+            
+            # Display audio player and download link
+            audio_file = open("speech.mp3",'rb')
+            audio_bytes = audio_file.read()
+            
+            st.text_area(label="Response:", value=response, height=350)
+            st.audio(audio_bytes,format='audio/mp3')
+            st.download_button(label="Download Speech",
+                               data=audio_bytes,
+                               file_name="speech.mp3",
+                               mime="audio/mp3")
+            
+            
+
+# text = takeCommand()
+# res = gemini_model(text)
+# text_to_speech(res)
+
+main()
